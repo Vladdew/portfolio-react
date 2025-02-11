047cbef86de1a02905ebb379a650112583b5d6a4
user <pixel22yuliya@gmail.com>
2025-02-11 12:26:20 +0100

    extended overlay added

--- src/components/Content/view/Heft/Heft.tsx
+++ src/components/Content/view/Heft/Heft.tsx
@@ -5,43 +5,64 @@ import { useTranslation } from "react-i18next";
 
 const Heft = (props: { onClick: () => void; isCv: boolean | "1" }) => {
   const [showIntro1, setShowIntro1] = useState<boolean>(false);
+  const [hideOverlay, setHideOverlay] = useState<boolean>(false);
+
   const { t } = useTranslation();
 
+  const overlayVivibilityHandler = () => {
+    if (props.isCv) {
+      setHideOverlay(true);
+    }
+
+    if (!props.isCv) {
+      setTimeout(() => {
+        setHideOverlay(false);
+      }, 500);
+    }
+  };
+
+  const onOverlayClick = () => {
+    overlayVivibilityHandler();
+    if (props.isCv === "1") {
+      setTimeout(() => {
+        setShowIntro1(true);
+      }, 1000);
+    }
+    props.onClick();
+  };
+
   return (
-    <div
-      className={
-        props.isCv === "1"
-          ? "heft"
-          : props.isCv
-          ? "heft animate-out"
-          : "heft animate-in"
-      }
-    >
-      <CvPDF showIntro1={showIntro1} isCv={props.isCv} />
+    <div className="heftContainer">
       <div
-        onClick={() => {
-          if (props.isCv === "1") {
-            setTimeout(() => {
-              setShowIntro1(true);
-            }, 1500);
-          }
-          props.onClick();
-        }}
-        className="overlay"
+        className={
+          props.isCv === "1"
+            ? "heft"
+            : props.isCv
+            ? "heft animate-out"
+            : "heft animate-in"
+        }
       >
-        <span className="heft__span">{t("heft.over")}</span>
-        <img src={Hr1} alt="" />
+        <CvPDF showIntro1={showIntro1} isCv={props.isCv} />
+
+        <div className="image-part-bottom"></div>
+        <button
+          onClick={() => {
+            overlayVivibilityHandler();
+            props.onClick();
+          }}
+          className="heft__cls cls"
+          type="button"
+        >
+          &#10006;
+        </button>
       </div>
-      <div className="image-part-bottom"></div>
-      <button
-        onClick={() => {
-          props.onClick();
-        }}
-        className="heft__cls cls"
-        type="button"
+      <div
+        onClick={onOverlayClick}
+        className={`overlay ${hideOverlay ? "overlay-hidden" : ""}`}
       >
-        &#10006;
-      </button>
+        <span className="overlay__span">{t("heft.over")}</span>
+        <img className="overlay__img" src={Hr1} alt="" />
+      </div>
     </div>
   );
 };
